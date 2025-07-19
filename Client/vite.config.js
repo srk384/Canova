f31ff2d9c1e378a8c7ee8de93f674348f2054f8d
srk384 <khanshahrukh384@gmail.com>
2025-07-20 01:48:59 +0530

    feat: Configure Vite server settings for improved accessibility

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allows access from other devices
+    port: 5173,       // Use your preferred port
+  },
 })
